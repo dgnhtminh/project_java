22ff65f9d6fa4702a82d2631559f4b44d00e0b7e
Caito Vũ <118267358+chorono181204@users.noreply.github.com>
2024-10-07 00:24:01 +0700

    Update JDBCConect.java

--- src/java/util/JDBCConect.java
+++ src/java/util/JDBCConect.java
@@ -4,10 +4,26 @@
  */
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Hacom
  */
 public class JDBCConect {
-    
+    static final String DB_URL = "jdbc:mysql://localhost:3306/rentcar";
+	static final String USER = "root";
+	static final String PASS = "123456";
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
 }
